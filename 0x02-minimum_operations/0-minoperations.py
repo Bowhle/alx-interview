a1675c012effb6be3e38a2eab2b29e1f8abafed7
bowhle <bowhlem@gmailcom>
2025-05-02 14:18:49 +0200

    Pycodestyle corrections

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 """
-Module to calculate the minimum number of operations needed to result in exactly n H characters.
+Module to calculate the minimum number of operations
+needed to result in exactly n H characters.
 """
 
 def minOperations(n):
     """
-    Calculate the fewest number of operations needed to result in exactly n H characters.
+    Calculate the fewest number of operations needed
+    to result in exactly n H characters.
     
     Args:
         n (int): The target number of H characters.
     
     Returns:
-        int: The minimum number of operations needed, or 0 if n is impossible to achieve.
+        int: The minimum number of operations needed,
+        or 0 if n is impossible to achieve.
     """
     if n <= 1:
         return 0
